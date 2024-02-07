b84094684d239906164d1940e86eeb054719c19c
JeusBacani <154034367+JeusBacani@users.noreply.github.com>
2024-02-07 11:35:26 +0800

    Update AbbreviateTwoWords.java

--- AbbreviateTwoWords.java
+++ AbbreviateTwoWords.java
@@ -1,10 +1,21 @@
-public class AbbreviateTwoWords {
+// AbbreviateTwoWords.java
+public final class AbbreviateTwoWords {
 
-  public static String abbrevName(String name) {
-    name = name.toUpperCase();
-    
-    String[] names = name.split(" ");
-    
-    return names[0].charAt(0)+"."+names[1].charAt(0);
-  }
+    private AbbreviateTwoWords() {
+        throw new UnsupportedOperationException("Utility class should not be instantiated.");
+    }
+
+    /**
+     * Abbreviates a name by taking the first letter of each word and separating them with a dot.
+     *
+     * @param name The full name.
+     * @return The abbreviated name.
+     */
+    public static String abbrevName(final String name) {
+        final String[] names = name.toUpperCase().split(" ");
+        if (names.length < 2) {
+            throw new IllegalArgumentException("The name must consist of at least two words.");
+        }
+        return names[0].charAt(0) + "." + names[1].charAt(0);
+    }
 }
